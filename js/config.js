2c0c599fcf1a0f726884177f46ea19dc8667149e
John Kponyo <71002981+nullstead@users.noreply.github.com>
2025-04-18 09:06:36 +0000

    Update config.js
    Updated congnito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_ahoOilK9J', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '20m7oel7ja6vg3jkh4u8jjajfk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
